53b9b139426577bfa7a590d39fdcc9b0ce5304b9
José Nunes <jnunesds2@gmail.com>
2022-06-26 10:59:01 -0300

    Criando a função focus no input

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-const inputs = document.querySelectorAll('.input');
-inputs[0].addEventListener('click', () => console.log('testando'));
+const inputs = document.querySelectorAll('.input');/*Essa linha é a declaração da variavel inputs, tudo dentro do input vai para "inputs"*/
+const hardleFocus = ({target}) => {
+    const span = target.previousElementSibling;
+    span.classList.add('span-active');
+}
+const hardleFocusout = ({target}) => {
+
+    if(target.value == ''){/*função if, se o target que é o input estiver vazio, então pode voltar ao estado anterior*/
+    const span = target.previousElementSibling;
+    span.classList.remove('span-active');
+    }
+}
+inputs.forEach ((input) => input.addEventListener ('focus', hardleFocus)
+);
+inputs.forEach ((input) => input.addEventListener ('focusout', hardleFocusout)
+);
