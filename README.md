77e19a66fd68dd92ff5ad2ccbb7cd6c61f54a151
José Nunes <jnunesds2@gmail.com>
2022-06-27 22:52:39 -0300

    Projeto finalizado

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Tela de login do League of Legends
 
-momento da aula -- 20:54
+<img src="imagens/Screenshot_2.png" alt="">
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const inputs = document.querySelectorAll('.input');/*Essa linha é a declaração da variavel inputs, tudo dentro do input vai para "inputs"*/
+const button = document.querySelector('.bnt-login');
 const hardleFocus = ({target}) => {
     const span = target.previousElementSibling;
     span.classList.add('span-active');
@@ -10,7 +11,19 @@ const hardleFocusout = ({target}) => {
     span.classList.remove('span-active');
     }
 }
+const handleChange = () => {
+    const [username, password] = inputs;
+
+    if(username.value && password.value.length >= 8){
+        button.removeAttribute('disabled');
+    }else{
+        button.setAttribute('disabled', '');
+    }
+
+}
 inputs.forEach ((input) => input.addEventListener ('focus', hardleFocus)
 );
 inputs.forEach ((input) => input.addEventListener ('focusout', hardleFocusout)
+);
+inputs.forEach ((input) => input.addEventListener ('input', handleChange)
 );
